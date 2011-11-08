bee83155ab507ec3430e541dc7595e6a9b4d4d0b
Max Degterev <me@maxdegterev.name>
2011-11-09 01:09:55 +0400

    converted into obj

--- static/script.js
+++ static/script.js
@@ -1,59 +1,18 @@
-    var timer = $('#clock_timer'),
-        spinners = {},
-        comp, step,// max,
-        duration = 200,
-        date_parts = timer.attr('datetime').split(':'),
-        spinner_html = '<ul>' +
-                            '<li>9</li>' +
-                            '<li>0</li>' +
-                            '<li>1</li>' +
-                            '<li>2</li>' +
-                            '<li>3</li>' +
-                            '<li>4</li>' +
-                            '<li>5</li>' +
-                            '<li>6</li>' +
-                            '<li>7</li>' +
-                            '<li>8</li>' +
-                            '<li>9</li>' +
-                            '<li>0</li>' +
-                            '<li>1</li>' +
-                            '<li>2</li>' +
-                            '<li>3</li>' +
-                            '<li>4</li>' +
-                            '<li>5</li>' +
-                            '<li>6</li>' +
-                            '<li>7</li>' +
-                            '<li>8</li>' +
-                            '<li>9</li>' +
-                        '</ul>';
-
-    var getEstTime = function(est) {
-        var fracs = [
-                (24 * 60 * 60 * 1000),
-                (60 * 60 * 1000),
-                (60 * 1000),
-                (1000)
-            ],
-            res = [],
-            i = 0,
-            l = fracs.length;
-
-        for (; i < l; i++) {
-            if (i > 0) {
-                est = est - fracs[i - 1];
-            }
-
-            res.push(est / fracs[i]);
-        }
-
-        $.map(res, function(val, i) {
-            return (val | 0);
-        });
-
-        return res;
-    };
+Timer = {
+    spinners: {},
+    duration: 200,
     
-    var setFragment = function(el, val) {
+    parseTimeStamp: function(ts) {
+
+        return {
+            days: [0, 1],
+            hours: [1, 4],
+            minutes: [2, 2],
+            seconds: [1, 6]
+        };
+    },
+    
+    setFragment: function(el, val) {
         var curr = el.data('curr'),
             leap = (val < curr),
             next = -(step * (val + (leap ? 10 : 0))) + comp;
@@ -64,49 +23,71 @@
 
         el.animate({
             top: next
-        }, duration, leap ? function() {
+        }, Timer.duration, leap ? function() {
             this.style.top = (-(step * val) + comp) + 'px';
         } : undefined);
 
         el.data('curr', val)
-    };
+    },
     
-    var setSpinner = function(seg, vals) {
+    setSpinner: function(seg, vals) {
         var l = seg.length,
             i = 0;
 
         for (; i < l; i++) {
-            setFragment(seg.eq(i), vals[i]);
+            Timer.setFragment(seg.eq(i), vals[i]);
         }
-    };
-
-    var updateStructure = function() {
-        var wrap = timer.parent(),
-            frag = timer.detach(),
-            segments = {
-                days: timer.find('.days'),
-                hours: timer.find('.hours'),
-                minutes: timer.find('.minutes'),
-                seconds: timer.find('.seconds')
-            };
-        
-        $.each(segments, function(key, el) {
-            el.html(spinner_html+spinner_html);
-            spinners[key] = segments[key].find('ul');
-            spinners[key].eq(1).addClass('second');
-            spinners[key].data('curr', 0);
+    },
+    
+    startCountdown: function() {
+        Timer.timestamp--;
+        Timer.time = Timer.parseTimeStamp(Timer.timestamp);
+        $.each(Timer.spinners, function(key, el) {
+            Timer.setSpinner(el, Timer.time[key])
         });
+        setTimeout(Timer.startCountdown, 1000);
+    }
+};
+    
+Timer.init = function() {
+    var timer = $('#clock_timer'),
+        wrap = timer.parent(),
+        frag = timer.detach(),
+        segments = {
+            days: timer.find('.days'),
+            hours: timer.find('.hours'),
+            minutes: timer.find('.minutes'),
+            seconds: timer.find('.seconds')
+        },
+        spinner_html = '<ul><li>9</li>';
 
-        wrap.append(timer);
-        
-        comp = parseInt(spinners.days.eq(0).css('top'), 10);
-        step = spinners.days.find('li').eq(0).height();
-        
-        //max = spinners.days.eq(0).height() - step;
-        
-        $.each(spinners, function(key, el) {
-            //setSpinner()
-        });
-    }();
+    for (var x = 0; x < 2; x++) {
+        for (var y = 0; y < 10; y++) {
+            spinner_html += '<li>' + y + '</li>'
+        }
+    }
+    spinner_html += '</ul>';
+    
+    Timer.timestamp = timer.data('est');
+    Timer.time = Timer.parseTimeStamp(Timer.timestamp);
+
+    $.each(segments, function(key, el) {
+        el.html(spinner_html + spinner_html);
+        Timer.spinners[key] = segments[key].find('ul');
+        Timer.spinners[key].eq(1).addClass('second');
+        Timer.spinners[key].data('curr', 0);
+    });
+
+    wrap.append(timer);
+    
+    comp = parseInt(Timer.spinners.days.eq(0).css('top'), 10);
+    step = Timer.spinners.days.find('li').eq(0).height();
+    
+    $.each(Timer.spinners, function(key, el) {
+        Timer.setSpinner(el, Timer.time[key])
+    });
+    
+    Timer.startCountdown();
+}();
 
 
