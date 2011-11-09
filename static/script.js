52269159277780444a8c49abfd78d9aa16a300ec
Max Degterev <max@ostrovok.ru>
2011-11-09 15:28:09 +0400

    jQueryness

--- static/script.js
+++ static/script.js
@@ -1,82 +1,15 @@
-Timer = {
-    spinners: [],
-    duration: 200,
-    countdown: true,
-
-    parseTimeStamp: function(ts) {
-        var divisors = [
-                24 * 60 * 60,
-                60 * 60,
-                60,
-                1
-            ],
-        i = 0,
-        j = divisors.length,
-        x,
-        res = [];
-
-        for (; i < j; i++) {
-            x = (ts / divisors[i]) | 0;
-            ts -= (divisors[i] * x);
-            x = ('0' + x).slice(-2);
-            res.push([+x.charAt(0), +x.charAt(1)]);
-        }
-
-        return res;
-    },
-
-    setFragment: function(el, val) {
-        var curr = el.data('curr'),
-            leap, next;
-
-        if (val === curr) { // LOLWTF!
-            return;
-        }
-
-        if (Timer.countdown) {
-            leap = (val > curr);
-            next = -(step * (9 - val + (leap ? 10 : 0))) + comp;
-
-            el.animate({
-                top: next
-            }, Timer.duration, leap ? function() {
-                this.style.top = (-(step * (9 - val)) + comp) + 'px';
-            } : undefined).data('curr', val);
-        }
-        else {
-            leap = (val < curr);
-            next = -(step * (val + (leap ? 10 : 0))) + comp;
-
-            el.animate({
-                top: next
-            }, Timer.duration, leap ? function() {
-                this.style.top = (-(step * val) + comp) + 'px';
-            } : undefined).data('curr', val);
-        }
-    },
-
-    setSpinner: function(seg, vals) {
-        var i = 0,
-            j = seg.length;
-
-        for (; i < j; i++) {
-            Timer.setFragment(seg.eq(i), vals[i]);
-        }
-    },
-
-    startCountdown: function() {
-        setTimeout(Timer.startCountdown, 1000);
-        Timer.countdown ? (Timer.timestamp--) : (Timer.timestamp++);
-        Timer.time = Timer.parseTimeStamp(Timer.timestamp);
-        for (var i = 0, j = Timer.spinners.length; i < j; i++) {
-            Timer.setSpinner(Timer.spinners[i], Timer.time[i]);
-        }
-    }
-};
-
-Timer.init = function() {
-    var timer = $('#clock_timer'),
-        wrap = timer.parent(),
+var Timer = function (el, options) {
+    this.options = options || {
+        duration: 200,
+        countdown: true
+    };
+    
+    // sane options
+    (this.options.duration > 800) && (this.options.duration = 800);
+    
+    this.spinners = [];
+    
+    var timer = el.find('.timer')
         i = 0,
         j = 0,
         segments = [
@@ -87,7 +20,7 @@ Timer.init = function() {
         ],
         spinner_html = '<ul>';
 
-    if (Timer.countdown) {
+    if (this.options.countdown) {
         spinner_html += '<li>0</li>'
         for (i = 0; i < 2; i++) {
             for (j = 9; j >= 0; j--) {
@@ -107,26 +40,124 @@ Timer.init = function() {
     spinner_html += '</ul>';
 
     // Set initial values
-    Timer.timestamp = (+timer.data('est') / 1000) | 0;
-    Timer.time = Timer.parseTimeStamp(Timer.timestamp);
+    this.timestamp = (+timer.data('est') / 1000) | 0;
+    this.time = this.parseTimeStamp(this.timestamp);
 
     // Create spinners
     timer.detach();
 
     for (i = 0, j = segments.length; i < j; i++) {
         segments[i].html(spinner_html + spinner_html);
-        Timer.spinners[i] = segments[i].find('ul');
-        Timer.spinners[i].eq(1).addClass('col2');
-        Timer.spinners[i].data('curr', Timer.countdown ? 9 : 0);
+        this.spinners[i] = segments[i].find('ul');
+        this.spinners[i].eq(1).addClass('col2');
+        this.spinners[i].data('curr', this.options.countdown ? 9 : 0);
     }
 
-    wrap.append(timer);
+    el.append(timer);
 
     // Calculate offsets
-    comp = parseInt(Timer.spinners[0].eq(0).css('top'), 10);
-    step = Timer.spinners[0].find('li').eq(0).height();
+    comp = parseInt(this.spinners[0].eq(0).css('top'), 10);
+    step = this.spinners[0].find('li').eq(0).height();
 
-    Timer.startCountdown();
-}();
+    this.startCountdown();
+    
+    return this;
+};
 
+// method example
+Timer.prototype.set = function(options) {
+    this.options = $.extend(this.options. options);
+    return this;
+};
+
+Timer.prototype.parseTimeStamp = function(ts) {
+    var divisors = [
+            24 * 60 * 60,
+            60 * 60,
+            60,
+            1
+        ],
+    i = 0,
+    j = divisors.length,
+    x,
+    res = [];
+
+    for (; i < j; i++) {
+        x = (ts / divisors[i]) | 0;
+        ts -= (divisors[i] * x);
+        x = ('0' + x).slice(-2);
+        res.push([+x.charAt(0), +x.charAt(1)]);
+    }
+
+    return res;
+};
+
+Timer.prototype.setFragment = function(el, val) {
+    var curr = el.data('curr'),
+        leap, next;
+
+    if (val === curr) { // LOLWTF!
+        return;
+    }
+
+    if (this.options.countdown) {
+        leap = (val > curr);
+        next = -(step * (9 - val + (leap ? 10 : 0))) + comp;
+
+        el.animate({
+            top: next
+        }, this.options.duration, leap ? function() {
+            this.style.top = (-(step * (9 - val)) + comp) + 'px';
+        } : undefined).data('curr', val);
+    }
+    else {
+        leap = (val < curr);
+        next = -(step * (val + (leap ? 10 : 0))) + comp;
+
+        el.animate({
+            top: next
+        }, this.options.duration, leap ? function() {
+            this.style.top = (-(step * val) + comp) + 'px';
+        } : undefined).data('curr', val);
+    }
+};
+
+Timer.prototype.setSpinner = function(seg, vals) {
+    var i = 0,
+        j = seg.length;
+
+    for (; i < j; i++) {
+        this.setFragment(seg.eq(i), vals[i]);
+    }
+};
+
+Timer.prototype.startCountdown = function() {
+    setTimeout($.proxy(this.startCountdown, this), 1000);
+    this.options.countdown ? (this.timestamp--) : (this.timestamp++);
+    this.time = this.parseTimeStamp(this.timestamp);
+    for (var i = 0, j = this.spinners.length; i < j; i++) {
+        this.setSpinner(this.spinners[i], this.time[i]);
+    }
+};
+
+$.fn.timer = function(options) {
+    var els = $(this);
+    
+    els.each(function() {
+        var el = $(this),
+            timer = el.data('timer');
+        
+        if(!timer)
+        {
+            el.data('timer', new Timer(el, options));
+        }
+        else if(options)
+        {
+            timer.set(options)
+        }
+        
+    });
+    
+    return els.eq(0).data('timer');
+};
 
